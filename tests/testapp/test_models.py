369e2c0f7b38a1ded5feff16a10f520abaf7e74c
Neftali Acosta <neftaliacosta@timeout.com>
2018-10-25 14:18:26 +0000

    Added test model to add condition to a switch

--- tests/testapp/test_models.py
+++ tests/testapp/test_models.py
@@ -3,6 +3,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import django
 from django.core.management import call_command
+from django.test import TestCase
+
+from gargoyle import gargoyle
+from gargoyle.constants import AB_TEST
+from gargoyle.models import Switch
 
 
 def test_no_migrations_required(db):
@@ -18,3 +23,25 @@ def test_no_migrations_required(db):
             pass
         else:
             raise AssertionError('Migrations required')
+
+
+class SwitchTest(TestCase):
+
+    def test_add_condition(self):
+        switch = Switch.objects.create(key='key1')
+        switch_data = switch.to_dict(manager=gargoyle)
+        assert len(switch_data['conditions']) == 0
+
+        switch.add_condition(gargoyle, 'gargoyle.builtins.IPAddressConditionSet', 'ip_address', '1.1.1.1',
+                             condition_type=AB_TEST)
+
+        switch = Switch.objects.get(key='key1')
+        switch_data = switch.to_dict(manager=gargoyle)
+        assert len(switch_data['conditions']) == 1
+        name, value, field_value, exclude, condition_type = switch_data['conditions'][0]['conditions'][0]
+        assert name == 'ip_address'
+        assert value == '1.1.1.1'
+        assert field_value == '1.1.1.1'
+        assert exclude is False
+        assert condition_type == AB_TEST
+
